23c4c25d25010ba37a9081cbc292f9f70c7b16c1
kiran tamang <35340684+12kirantamang@users.noreply.github.com>
2026-05-02 14:58:49 +0900

    Fix formatting in TEMPLATES settings

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # Add this line
+        'DIRS': [BASE_DIR / 'templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
